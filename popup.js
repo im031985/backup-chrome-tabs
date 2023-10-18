e7a2963550758e7bc3ab36bf4e14a154c6d06764
Ibrahim Abdelmagid <im031985@gmail.com>
2023-10-18 15:10:50 +0300

    save backup json to disk

--- popup.js
+++ popup.js
@@ -1,14 +1,18 @@
 console.log("This is popup js")
 const backupBtn = document.querySelector('#backup')
 backupBtn.addEventListener('click', async () => {
-    let windows = await chrome.windows.getAll()
-    console.log(windows)
-    for (let i = 0; i < windows.length; i++){
-        let window = windows[i]
+    let backupObj = await chrome.windows.getAll()
+    console.log(backupObj)
+    for (let i = 0; i < backupObj.length; i++){
+        let window = backupObj[i]
         let windowTabs = await chrome.tabs.query({windowId: window.id})
-        windows[i]["tabs"] = windowTabs
+        backupObj[i]["tabs"] = windowTabs
     }
+    let text = JSON.stringify(backupObj)
+    var blob = new Blob([text],{type:"text/json;charset=utf-8"})
+    saveAs(blob, "chrome-backup.json")
     console.log("Windows with tabs")
-    console.log(windows)
+    console.log(backupObj)
     console.log("done")
+    doExportToDisk()
 })
